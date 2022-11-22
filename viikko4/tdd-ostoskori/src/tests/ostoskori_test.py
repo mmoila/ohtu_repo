1e873d931787a2ef231b5498bf3a40d1769084e8
Mikael Moilanen <mikael.moilanen@gmail.com>
2022-11-22 17:15:46 +0200

    Lisaa testi jonka mukaan kahden eri ostoksen lisayksen jalkeen ostoskorin sisalto on oikeanlainen

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -68,3 +68,13 @@ class TestOstoskori(unittest.TestCase):
 
         self.assertEqual(ostos.tuotteen_nimi(), "Maito")
         self.assertEqual(ostos.lukumaara(), 1)
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korissa_on_kaksi_eri_ostosta(self):
+        maito = Tuote("Maito", 3)
+        suklaa = Tuote("Suklaa", 4)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(suklaa)
+
+        ostokset = self.kori.ostokset()
+
+        self.assertEqual(len(ostokset), 2)
